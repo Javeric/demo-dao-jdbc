e91d3fefb9ca5551bfc0f80286fb4f2ee95285fa
Eric <ericrangel13@hotmail.com>
2020-04-22 10:32:08 -0300

    FindById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,20 @@
 package model.dao;
 
-public class DaoFactory {
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
 
-}
+public class DaoFactory { 
+		
+		
+	public static SellerDao createSellerDao() {
+			return new SellerDaoJDBC(DB.getConnection());
+	
+	}
+	}
+
+//classe com operações estáticas
+//para instanciar os DAOs
+//método que retorna um tipo da interface, mas internamente
+//retorna a implementação. Não exponho a implement., só a interface
+//agora vamos lá no Program e ao inves de instanciar um objeto SellerDao
+//com o construtor, usando o new, eu só chamo esse método.
